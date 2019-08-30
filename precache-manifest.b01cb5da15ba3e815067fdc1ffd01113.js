fb24fc8e789d6235c6e891c86744752d97c63190
Erik Shumkov <erikshumkov@gmail.com>
2019-08-30 21:42:17 +0200

    Updates

---
+++ precache-manifest.b01cb5da15ba3e815067fdc1ffd01113.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f61e8f90ef288bcc9a0d9120a72245c",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "1b3cb1c1693159ca3bb8",
+    "url": "/todo/static/css/main.1928e99a.chunk.css"
+  },
+  {
+    "revision": "b7803575ab9f197c0976",
+    "url": "/todo/static/js/2.bdca694c.chunk.js"
+  },
+  {
+    "revision": "1b3cb1c1693159ca3bb8",
+    "url": "/todo/static/js/main.d8b39521.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  }
+]);
